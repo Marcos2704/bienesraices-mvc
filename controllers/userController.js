3d4a77e55b20510dc55ab340cb05548edcbe9714
Marcos2704 <nahuelmarcos2001@gmail.com>
2022-09-21 07:24:55 -0300

    create controllers and endpoints

---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+const loginForm = (req, res) =>{
+        res.render("auth/login", {
+          
+        } ) 
+    }
+
+const registerForm = (req, res) =>{
+        res.render("auth/register", {
+           
+        } )
+    }
+
+
+export {
+    loginForm,
+    registerForm
+}
