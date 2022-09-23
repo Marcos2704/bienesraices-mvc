3eef2ad336e96631f0ec1eadb025a7b419f24ab2
Marcos2704 <nahuelmarcos2001@gmail.com>
2022-09-23 07:48:35 -0300

    create form and routing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,24 @@
 const loginForm = (req, res) =>{
         res.render("auth/login", {
-          
+          pagina: "Iniciar Sesion"
         } ) 
     }
 
 const registerForm = (req, res) =>{
         res.render("auth/register", {
-           
+           pagina: "Crear cuenta"
+        } )
+    }
+
+    const olvideForm = (req, res) =>{
+        res.render("auth/olvide-password", {
+            pagina: "Recupera tu acceso"
         } )
     }
 
 
 export {
     loginForm,
-    registerForm
+    registerForm,
+    olvideForm
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import  express  from "express";
-import { loginForm, registerForm } from "../controllers/userController.js";
+import { loginForm, registerForm, olvideForm} from "../controllers/userController.js";
 
 const router = express.Router()
 
 router.get("/login", loginForm)
 router.get("/register", registerForm)
+router.get("/olvide-password", olvideForm)
 
 
 export default router
